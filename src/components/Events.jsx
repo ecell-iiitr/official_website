854c30fc7ecab7c94d9015ce4a003bd77a1c41d4
Gyanendra Tiwari <mrgyan432@gmail.com>
2023-05-24 23:07:33 +0530

    swiper modified

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,12 +1,15 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectFlip, Autoplay, Navigation, Keyboard } from "swiper";
+import { EffectCube, Autoplay, Navigation, Keyboard } from "swiper";
 import 'swiper/css';
 import "swiper/css/navigation";
 import Slide from './EventSlide';
 
 const Events = () => {
   return (
-    <div id="events" className="bg-[#414141] scroll-m-12 py-8 sm:py-16 px-6 sm:px-12 lg:px-24">
+    <div
+      id="events"
+      className="bg-[#414141] scroll-m-12 py-8 sm:py-16 px-6 sm:px-12 lg:px-24"
+    >
       <h1 className="text-xl xs:text-2xl md:text-3xl lg:text-4xl pb-12 font-body text-center font-bold underline-offset-8 underline text-[#3498D9]">
         Our Events
       </h1>
@@ -16,7 +19,7 @@ const Events = () => {
         onSlideChange={() => console.log("slide change")}
         onSwiper={(swiper) => console.log(swiper)}
         centeredSlides={true}
-        effect={"flip"}
+        effect={"cube"}
         loop={true}
         autoplay={{
           delay: 5000,
@@ -24,14 +27,10 @@ const Events = () => {
         }}
         navigation={true}
         keyboard={true}
-        modules={[EffectFlip, Autoplay, Navigation, Keyboard]}
+        modules={[EffectCube, Autoplay, Navigation, Keyboard]}
       >
-        <SwiperSlide>
-          <Slide />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Slide />
-        </SwiperSlide>
+        <SwiperSlide> <Slide /> </SwiperSlide>
+        <SwiperSlide> <Slide /> </SwiperSlide>
       </Swiper>
     </div>
   );
